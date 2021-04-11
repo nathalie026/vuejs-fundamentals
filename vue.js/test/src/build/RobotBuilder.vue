fadf9752943c3ba32e793328f438a1538e8392c9
nathalie026 <nathalie.ton77@gmail.com>
2021-04-11 16:19:14 +0200

    passing data to the parent component from the child component with $emit using lifecycle to pass the data created and updated

--- vue.js/test/src/build/RobotBuilder.vue
+++ vue.js/test/src/build/RobotBuilder.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="content">
-    <button class="ad-to-cart" @click="addToCart()">
-      Add to cart
-    </button>
+
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
+      </div>
+      <button class="add-to-cart" @click="addToCart()">
+        Add to cart
+      </button>
+    </div>
+
     <div class="top-row">
       <PartSelector
         :parts="avaibleParts.heads"
         position= "top"
+        @partSelected= " part => selectedRobot.head = part"
         />
         <!-- <div class="robot-name">
           {{selectedRobot.head.title}}
@@ -17,20 +35,24 @@
         <PartSelector
           :parts="avaibleParts.arms"
           position= "left"
+          @partSelected= " part => selectedRobot.leftArm = part"
         />
         <PartSelector
           :parts="avaibleParts.torsos"
           position= "center"
+          @partSelected= " part => selectedRobot.torso = part"
         />
         <PartSelector
           :parts="avaibleParts.arms"
           position= "right"
+          @partSelected= " part => selectedRobot.rightArm = part"
           />
     </div>
     <div class="bottom-row">
       <PartSelector
         :parts="avaibleParts.bases"
         position= "bottom"
+        @partSelected= " part => selectedRobot.base = part"
         />
     </div>
     <div>
@@ -102,17 +124,7 @@ export default {
 
 </script>
 <style lang="scss" scoped>
-.part {
-  position: relative;
-  width:165px;
-  height:165px;
-  border: 3px solid #aaa;
-}
-.part {
-  img {
-    width:165px;
-  }
-}
+
 .top-row {
   display:flex;
   justify-content: space-around;
@@ -126,72 +138,6 @@ export default {
   justify-content: space-around;
   border-top: none;
 }
-.head {
-  border-bottom: none;
-}
-.left {
-  border-right: none;
-}
-.right {
-  border-left: none;
-}
-.left img {
-  transform: rotate(-90deg);
-}
-.right img {
-  transform: rotate(90deg);
-}
-.bottom {
-  border-top: none;
-}
-.prev-selector {
-  position: absolute;
-  z-index:1;
-  top: -3px;
-  left: -28px;
-  width: 25px;
-  height: 171px;
-}
-.next-selector {
-  position: absolute;
-  z-index:1;
-  top: -3px;
-  right: -28px;
-  width: 25px;
-  height: 171px;
-}
-.center .prev-selector, .center .next-selector {
-  opacity:0.8;
-}
-.left .prev-selector {
-  top: -28px;
-  left: -3px;
-  width: 144px;
-  height: 25px;
-}
-.left .next-selector {
-  top: auto;
-  bottom: -28px;
-  left: -3px;
-  width: 144px;
-  height: 25px;
-}
-.right .prev-selector {
-  top: -28px;
-  left: 24px;
-  width: 144px;
-  height: 25px;
-}
-.right .next-selector {
-  top: auto;
-  bottom: -28px;
-  left: 24px;
-  width: 144px;
-  height: 25px;
-}
-.right .next-selector {
-  right: -3px;
-}
 .robot-name {
   position: absolute;
   top: -25px;
@@ -199,19 +145,15 @@ export default {
   width: 100%;
   font-size: 15px;
 }
-.sale {
-  color: white;
-  background: red;
-}
 .content {
   position: relative;
 }
 .add-to-cart {
   position: absolute;
-  right: 30px;
-  width: 220px;
+  width: 210px;
   padding: 3px;
   font-size: 16px;
+
 }
 td, th{
   text-align: left;
@@ -220,5 +162,26 @@ td, th{
 }
 .cost{
   text-align: right;
+}
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 210px;
+  height: 210px;
+  padding: 5px;
+}
+.preview-content {
+  border: 1px solid #999;
+}
+.preview img {
+  width: 50px;
+  height: 50px;
+}
+.rotate-right {
+  transform: rotate(90deg);
+}
+.rotate-left {
+  transform: rotate(-90deg);
 }
 </style>
